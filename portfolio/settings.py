07383fda0fd1970ac22fe7fab8ef09335a18c7e5
Carlos Argueta <carlosargueta@CarlosArgueta.local>
2020-04-11 12:33:17 -0600

    password change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'Realmadrid#25!',
+        'PASSWORD': 'Capital#1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
